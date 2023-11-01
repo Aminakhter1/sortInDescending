fd16c7224e32f685a88b6ecfa7b4058b5c4fbdfb
AMIN AKHTER <120783238+Aminakhter1@users.noreply.github.com>
2023-11-01 12:34:19 +0530

    Update sortInDescending.js
    i changed built in function sort

--- sortInDescending.js
+++ sortInDescending.js
@@ -1,5 +1,17 @@
-const arr = [5, 8, 9, 1, 5, 2];
+function SortInDescending(arr) {
+  const n = arr.length;
 
-arr.sort((a, b) => b - a);
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+const numbers = [5, 2, 9, 1, 5, 6];
+SortInDescending(numbers);
+console.log(numbers);
 
-console.log(arr);
